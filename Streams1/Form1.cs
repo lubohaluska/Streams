810df592f4881089288546ebd3176a38b43ed10a
CSharpFri <CSharpFri@CLASS-2-06>
2018-02-02 20:50:44 +0100

    Add project files.

---
+++ Streams1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Streams1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Vyber soubor";
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.Filter = "Text files (TXT) | *.TXT";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                // Streamreader pro cteni streamu ze souboru
+                // using blog pro automaticke uvolneni streamu a zavretie otvoreneho souboru
+
+
+                using (StreamReader reader = new StreamReader(openFileDialog1.FileName))
+                {
+                    string obsahSouboru = reader.ReadToEnd();
+                    textBox2.Text = obsahSouboru;
+
+                }
+
+
+
+            }
+
+        }
+    }
+}
